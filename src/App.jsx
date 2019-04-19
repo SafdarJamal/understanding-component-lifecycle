0dcbbf9326343ce96d541dd82d008ee653752ef0
Safdar Jamal <cleverprogrammer.me@gmail.com>
2019-04-19 14:21:49 +0500

    task 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import PrimaryButton from './components/PrimaryButton.jsx';
-import SecondaryButton from './components/SecondaryButton.jsx';
-import Social from './components/Social.jsx';
-import UIButton from './components/UIButton.jsx';
+// import logo from './logo.svg';
+// import PrimaryButton from './components/PrimaryButton.jsx';
+// import SecondaryButton from './components/SecondaryButton.jsx';
+// import Social from './components/Social.jsx';
+// import UIButton from './components/UIButton.jsx';
+import Kid from './components/Kid.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      volume: 0
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return { volume: 5 };
+  }
+
   render() {
+    const { volume } = this.state;
+    console.log(volume);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <PrimaryButton />
-          <br />
-          <SecondaryButton />
-          <br />
-          <br />
-          <Social />
-          <br />
-          <br />
-          <UIButton title="Hello React" />
+          <Kid dressColor="green" />
         </header>
       </div>
     );
---
+++ src/components/Kid.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class Kid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emotion: 'nervous',
+      danceSteps: [],
+      currentStepIndex: 0,
+      startedPerforming: false
+    };
+  }
+
+  qualified() {
+    this.setState({ startedPerforming: false });
+  }
+
+  render() {
+    const { dressColor } = this.props;
+    const {
+      danceSteps,
+      emotion,
+      startedPerforming,
+      currentStepIndex
+    } = this.state;
+
+    return (
+      <div>
+        <div>dressColor: {dressColor}</div>
+        <div style={{ backgroundColor: dressColor, width: 50, height: 50 }} />
+        <div>Emotion: {emotion}</div>
+        {startedPerforming && (
+          <div>
+            <div>Current Step: {danceSteps[currentStepIndex]}</div>
+            <button
+              onClick={() =>
+                this.setState({ currentStepIndex: currentStepIndex + 1 })
+              }
+            >
+              Perform Next Step
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+Kid.defaultProps = { dressColor: 'red', applaud: false, furtherSteps: [] };
+
+export default Kid;
